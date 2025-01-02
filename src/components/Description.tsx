7bf7cfe8a4ac01e6bd6a75ac77c816be9cd4b141
Goobby <gooby@gooby.space>
2025-01-02 23:22:23 +0100

    fix description

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -3,12 +3,14 @@ function Description() {
     <>
       <div className="description">
         <h1>Interface Viewer</h1>
-        Searchable WoW interface viewer for finding files to use in RP profiles.
-        Inspired by{" "}
-        <a href="https://trpguide.carrd.co/">
-          https://trpguide.carrd.co/
-        </a> &{" "}
+        Searchable WoW interface viewer for finding files to use in RP profiles. Inspired by{" "}
+        <a href="https://trpguide.carrd.co/">https://trpguide.carrd.co/</a> &{" "}
         <a href="https://trpsecrets.carrd.co/">https://trpsecrets.carrd.co/</a>
+        <br />
+        The copy button under the image will copy a TRP compatible tag of your image, set to the
+        image size so the image will be displayed in the correct size in TRP.
+        <br />
+        For an explanation on the copied text , click on one of the carrd links above.
       </div>
     </>
   );
